4eed1dfb88e55fa32bc38ea2f4769c06cd90b509
Saurabh Gupta <saurabhgupta1403@gmail.com>
2018-01-20 10:15:13 -0800

    adding R learning files

---
+++ r_practice_1.R
@@ -0,0 +1,32 @@
+data("faithful")
+
+#setwd("/Users/saurabg/saurabg/practice codes/r_learning/")
+#getwd()
+
+#reddit <- read.csv("reddit.csv")
+#summary(reddit)
+#head(reddit)
+
+L = list(name = 'john', age = 14, no.children = 2)
+names(L)
+L[2]
+
+id = data("iris")
+head(id)
+
+names(iris)
+iris[1,]
+head(iris, 4)
+top(iris)
+head(iris$Sepal.Length)
+iris$Sepal.Length[1:10]
+mean(iris$Sepal.Length)
+mean(iris, na.rm = True)
+
+subset(iris, iris$Sepal.Length < 5)
+head(iris$Species)
+
+length(iris$Species[iris$Species == "setosa"])
+summary(iris)
+
+colMeans(iris[1:4])
