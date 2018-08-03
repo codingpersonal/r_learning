5367db5d09ca7ae3b633e67328315a447afe34a3
Saurabh Gupta <saurabhgupta1403@gmail.com>
2018-08-02 23:28:22 -0700

    more r learning

--- r_practice_1.R
+++ r_practice_1.R
@@ -31,3 +31,33 @@ length(iris$Species[iris$Species == "setosa"])
 summary(iris)
 
 colMeans(iris[1:4])
+
+
+execution_arithmetic <- function(n) {
+  a = seq(1, n, by=1)
+  cube_a_sum = sum(a^3)
+  return(cube_a_sum)
+}
+
+execution_sapply <- function(n) {
+  a = seq(1, n, by=1)
+  return(sum(sapply(a, function(i) {return(i^3)})))
+}
+
+execution_loop <- function(n) {
+  a = seq(1, n, by = 1)
+  cube_sum_a = 0
+  for (i in a) {
+    cube_sum_a = cube_sum_a + i^3
+  }
+  return(cube_sum_a)
+}
+
+n = 5000000
+system.time(execution_sapply(n))
+system.time(execution_loop(n))
+system.time(execution_arithmetic(n))
+
+
+
+
